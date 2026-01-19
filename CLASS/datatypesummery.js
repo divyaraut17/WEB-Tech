be72ca7e31c58cd13c2994f850da7b1f7a897d48
Divya raut <divyaraut415@gmail.com>
2026-01-19 14:34:03 +0530

    new added

---
+++ CLASS/datatypesummery.js
@@ -0,0 +1,31 @@
+//non -primitive --> Array,function,object
+//unique id--> symbol
+
+// let id1 = Symbol("123")
+// console.log(id1);
+// const id2 = Symbol("123")
+// console.log(id === id2);
+
+//array
+let arr = [1,2,3,4]
+console.log(arr);
+
+//Objects
+let myObject = { name : "divya",age : 2, email : "divya@gmail.com"}
+console.log(myObject);
+console.log(myObject.name);
+console.log(myObject.age);
+console.log(myObject.email);
+
+//Function
+let afun = function myFun(){
+     console.log("divya");
+     
+}
+console.log(afun);
+
+function add(){
+    return 2+2
+}
+
+
