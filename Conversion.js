2d5e8de2f3265fc1133ec623fd0190f0ee2e03eb
divyaraut17 <divyaraut287@gmail.com>
2026-01-19 10:04:54 +0530

    Add files via upload

---
+++ Conversion.js
@@ -0,0 +1,52 @@
+// let score = "22"
+// let scoreforbool = true
+// let scorefornill = null
+// let scoreforundefine = undefined
+
+// console.log(typeof(score));
+// console.log(typeof(scoreforbool));
+// console.log(typeof(scorefornill));
+// console.log(typeof(scoreforundefine));
+
+
+//convert string to number
+let jj = "22"
+let ssconvert = Number(jj);
+console.log(typeof(ssconvert));
+
+
+//convert number to string
+let ss = 45
+let jjconvert = String(ss);
+console.log(typeof(jjconvert));
+
+
+let aa = "22abc"
+let bbconvert = Number(aa);
+console.log(bbconvert);
+//if there is only no in string then it grt converted into no
+//if a string contain no plus char then it give o/p as NaN string cannot convert to no
+
+//Arithmetic operator
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2*4);
+// console.log(4/2);
+// console.log(4%2);
+
+// //concatination
+// let str1 = "Hello"
+// let str2 = " divya"
+// console.log(str1 + str2);
+
+// NOTE --> javascript only  automatical converts string to  number in arithmatic operations
+//Conversions
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + "2");
+console.log(1 + 3 +"2");
+console.log((3 + 4) * 5 % 3);
+
+
+
+
